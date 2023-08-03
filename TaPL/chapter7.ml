971e60e7badaa89de98cb776086a690eadc6e8cf
omosan0627 <hodakaymj0627@yahoo.co.jp>
2023-08-04 03:17:45 +0900

    評価について

--- TaPL/chapter7.ml
+++ TaPL/chapter7.ml
@@ -11,4 +11,7 @@ let rec term_to_string (t) =
     | TmAbs t' -> "TmAbs " ^ term_to_string t'
     | TmApp (t1, t2) -> "TmApp " ^ term_to_string t1 ^ term_to_string t2;;
 let t = TmApp (TmVar(3), TmApp (TmVar (2), TmVar (6)));;
-printf "%s\n" (term_to_string t)
+printf "%s\n" (term_to_string t);;
+let fix = fun f -> (fun x -> f (fun y z -> x x y z)) (fun x -> f (fun y z -> x x y z));;
+let gcd = fun g a b -> if b = 0 then a else g b (a mod b);;
+printf "%d\n" ((fix gcd) 10 15);;
